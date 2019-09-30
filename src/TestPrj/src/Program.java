9f53b24e3666f00e4146badd62ecc65182a073b2
terry894 <terry894@naver.com>
2019-09-30 16:08:51 +0900

    수정

--- src/TestPrj/src/Program.java
+++ src/TestPrj/src/Program.java
@@ -5,6 +5,6 @@ import test1.ExamProgram;
 public class Program {
 	public static void main(String[] args) {
               ExamProgram.printSumAndAvg(12,3);
-              //khkim
+              //myeong
 	}
 }
